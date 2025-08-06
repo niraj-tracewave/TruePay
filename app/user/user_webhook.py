79fd2a77d8ac76ef39594b5883185f0acb0affcb
steffyjk <steffy.jk2018@gmail.com>
2025-08-06 20:17:37 +0530

    [ Feat ] : added Webhook Concept to handled all Event triggers from RazorPay Captured Sub.activation and updated status to activate

--- app/user/user_webhook.py
+++ app/user/user_webhook.py
@@ -1,92 +1,81 @@
-from fastapi import FastAPI, Request, Header
-from fastapi.responses import JSONResponse
+import json
 import hmac
 import hashlib
-import json
-
-app = FastAPI()
- 
-WEBHOOK_SECRET = "@eQq3b23y6f9_kr"  # Same as Razorpay dashboard
-
-from fastapi import APIRouter, Request, Query
-from models.user import User
+from db_domains.db import DBSession
+from models.razorpay import Subscription
+from fastapi import Request, APIRouter
+from fastapi.responses import JSONResponse
+from config import app_config
+from fastapi import APIRouter, Request
 
 router = APIRouter(prefix="/razorpay", tags=["RazorPay API's"])
 
-@router.post("/webhook/")
-async def razorpay_webhook(request: Request, x_razorpay_signature: str = Header(None)):
+@router.post("/webhook")
+async def razorpay_webhook(request: Request):
+    body = await request.body()
+
+    x_razorpay_signature = request.headers.get("X-Razorpay-Signature")
+
     try:
         headers = request.headers
-        print("Incoming Headers:", headers)
-        print("x_razorpay_signature:", x_razorpay_signature)
         body_bytes = await request.body()
         body_str = body_bytes.decode("utf-8")
-        print("Raw Body:", body_str)
 
         # Verify signature
         generated_sig = hmac.new(
-            WEBHOOK_SECRET.encode('utf-8'),
+            app_config.WEBHOOK_SECRET.encode('utf-8'),
             body_bytes,
             hashlib.sha256
         ).hexdigest()
 
-        print("Generated Signature:", generated_sig)
-        print("Received Signature:", x_razorpay_signature)
         if generated_sig != x_razorpay_signature:
             return JSONResponse(content={"status": "invalid signature"}, status_code=400)
 
         # Parse event data
         data = json.loads(body_str)
         event = data.get("event")
+        match event:
+            case "subscription.activated":
+                sub_id = data.get("payload").get(
+                    "subscription", {}).get("entity", {}).get("id")
+                print("sub_id", sub_id)
+                if sub_id:
+                    filters = [
+                        Subscription.razorpay_subscription_id == sub_id,
+                        Subscription.is_deleted == False
+                    ]
+                    with DBSession() as session:
+                        sub_data = (
+                            session.query(Subscription)
 
-        if event == "subscription.authenticated":
-            print("Subscription Authenticated:", data)
+                            .filter(*filters)
+                            .first()
+                        )
+                        print("sub_data", sub_data)
+                        if not sub_data:
+                            pass
+                        sub_data.status = "active"
+                        session.commit()
 
-        elif event == "subscription.charged":
-            print("EMI Charged:", data)
+                # subscription_id = data.get("payload", {}).get("subscription", {}).get("entity", {}).get("id")
+                # logger.info("--------authenticated", body)
+                # logger.info("Subscription Authenticated:", data)
+                # logger.info("Subscription ID:", subscription_id)
 
-        elif event == "payment.failed":
-            print("Payment Failed:", data)
+            # case "subscription.charged":
+            #     logger.info("--------charged %s", body)
+            #     print("EMI Charged:", data)
 
-        elif event == "subscription.completed":
-            print("Subscription Completed:", data)
+            # case "payment.failed":
+            #     print("Payment Failed:", data)
 
-        return JSONResponse(content={"status": "success"})
+            # case "subscription.completed":
+            #     logger.info("--------completed %s", body)
+            #     print("Subscription Completed:", data)
+
+            case _:
+                # logger.warning("Unhandled event: %s", event)
+                return JSONResponse(content={"status": "success"})
     except Exception as e:
         print("Webhook Error:", str(e))
         return JSONResponse(content={"status": "error"}, status_code=500)
-
-# @router.post("/webhook/")
-# async def razorpay_webhook(request: Request):
-#     try:
-#         body_bytes = await request.body()
-#         print("Raw Body Bytes:", body_bytes)
-#         body_str = body_bytes.decode("utf-8")
-#         print("Raw Body String (repr):", repr(body_str))
-#         print("Content-Length:", request.headers.get("content-length"))
-
-#         signature = request.headers.get("X-Razorpay-Signature")
-#         print("Received Signature:", signature)
-
-#         if not signature:
-#             return JSONResponse(content={"status": "missing signature"}, status_code=400)
-
-#         generated_sig = hmac.new(
-#             WEBHOOK_SECRET.encode('utf-8'),
-#             body_bytes,
-#             hashlib.sha256
-#         ).hexdigest()
-
-#         print("Generated Signature:", generated_sig)
-#         if generated_sig != signature:
-#             return JSONResponse(content={"status": "invalid signature"}, status_code=400)
-
-#         data = json.loads(body_str)
-#         event = data.get("event")
-
-#         # handle events...
-#         return JSONResponse(content={"status": "success"})
-
-#     except Exception as e:
-#         print("Webhook Error:", str(e))
-#         return JSONResponse(content={"status": "error"}, status_code=500)
