609df8095688d7c95eac72e00f124159cdcc3fe8
steffyjk <steffy.jk2018@gmail.com>
2025-08-06 14:49:15 +0530

    Optimized code regarding fetch LoanDetails

--- app/user/user_razorpay.py
+++ app/user/user_razorpay.py
@@ -1,15 +1,17 @@
 from fastapi import Request
-from models.razorpay import Plan, Subscription
+from fastapi import APIRouter, Depends
+from starlette import status
+from db_domains.db import DBSession
+from sqlalchemy.orm import selectinload
 from models.loan import LoanApplicant
+from models.razorpay import Plan, Subscription
 from services.plan_service import PlanService
 from services.subscription_service import SubscriptionService
 from services.loan_service.user_loan import UserLoanService
-from fastapi import APIRouter, Depends
-from services.dependencies import get_razorpay_service
 from services.razorpay_service import RazorpayService
+from services.dependencies import get_razorpay_service
 from schemas.razorpay_schema import CreatePlanSchema, CreateSubscriptionSchema
 from common.utils import calculate_emi_schedule
-from starlette import status
 
 
 router = APIRouter(prefix="/razorpay", tags=["RazorPay API's"])
@@ -20,24 +22,31 @@ sub_service = SubscriptionService(Subscription)
 
 
 @router.post("/create-razorpay-plan-sub/{applicant_id}")
-def create_emi_plan(request: Request, applicant_id: str, payload: CreatePlanSchema, service: RazorpayService = Depends(get_razorpay_service)):
+def create_emi_mandate(request: Request, applicant_id: str, payload: CreatePlanSchema, service: RazorpayService = Depends(get_razorpay_service)):
     user_state = getattr(request.state, "user", None)
     # NOTE: ADD LOAN PLAN AND SUBSCRIPTIONS HERE
     # Step-1: Fetch Loan Details
-    current_loan = loan_service.get_loan_application_details(
-        user_id=user_state.get("id"),
-        loan_application_id=applicant_id
-    )
-    loan_details = current_loan.get("data").get("loan_application")
+    filters = [
+        LoanApplicant.id == applicant_id,
+        LoanApplicant.is_deleted == False
+    ]
+    with DBSession() as session:
+        loan_details = (
+            session.query(LoanApplicant)
+            .options(
+                selectinload(LoanApplicant.approval_details),
+            )
+            .filter(*filters)
+            .first()
+        )
+
+    loan_approval_detail = loan_details.approval_details[0]
     # Step -2 Fetch Approved Loan
-    user_accepted_amount = loan_details.get("approval_details")[
-        0].get("user_accepted_amount")
-    approved_interest_rate = loan_details.get("approval_details")[
-        0].get("approved_interest_rate")
-    approved_tenure_months = loan_details.get("approval_details")[
-        0].get("approved_tenure_months")
-    approved_processing_fee = loan_details.get("approval_details")[
-        0].get("approved_processing_fee")
+    # if loan_approval_detail:
+    user_accepted_amount = loan_approval_detail.user_accepted_amount
+    approved_interest_rate = loan_approval_detail.approved_interest_rate
+    approved_tenure_months = loan_approval_detail.approved_tenure_months
+    approved_processing_fee = loan_approval_detail.approved_processing_fee
     # Step - 3 Calculate EMI
     emi_result = calculate_emi_schedule(
         loan_amount=user_accepted_amount,
@@ -52,20 +61,19 @@ def create_emi_plan(request: Request, applicant_id: str, payload: CreatePlanSche
         emi = data.get("monthly_emi")
     # Step-2 Create Plan Based On provided Loan
     created_plan = plan_service.add_plan(
-        applicant_id=loan_details.get("id"),
+        applicant_id=loan_details.id,
         user_id=user_state.get("id"),
         form_data={
             "period": "monthly",
             "interval": 1,
             "item": {
-                "name": f"{loan_details.get("loan_uid")}_" + "05082025",
+                "name": f"{loan_details.loan_uid}_" + "05082025",
                 "amount": emi*100,  # NOTE: RAZORPAY *100 LOGIC
                 "currency": "INR",
                 "description": f"₹{user_accepted_amount} loan at {approved_interest_rate}% interest for {approved_tenure_months} months"
             },
         }
     )
-    print("created_plan", created_plan)
     plan_db_obj = created_plan.get("data")
     # Step-3 After Creating Plan Create Subscription
     if created_plan.get("success") == True:
